12b8569e469723a811ccd947f40def6b3d8cb771
fiza222 <fizaparween112@gmail.com>
2026-04-03 19:38:05 +0530

    Add files via upload

---
+++ qns6.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+        using namespace std;
+
+        int main(){
+
+           int length,breadth,area;
+
+           cout<<"enter the length : ";
+           cin>>length;
+
+           cout<<"enter the breadth : ";
+           cin>>breadth;
+
+           area=length*breadth;
+
+           cout<<"area of rectangle is : "<<area;
+
+           return 0;
+
+
+
+
+
+
+
+          }
