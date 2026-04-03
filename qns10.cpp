189f1740e8f0606c6fa96e5ae850bf0459e28928
fiza222 <fizaparween112@gmail.com>
2026-04-03 19:47:42 +0530

    Add files via upload

---
+++ qns10.cpp
@@ -0,0 +1,34 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a,b,c,d,e,f;
+
+    cout<<"enter the value of a : ";
+    cin>>a;
+
+    cout<<"enter the value of b: ";
+    cin>>b;
+
+    c=a+b;
+    cout<<"addition = "<<c;
+    "\n";
+
+    d=a-b;
+    cout<<"subtraction = "<<d;
+
+    e=a*b;
+    cout<<"multiplication ="<<e;
+
+    f=a/b;
+    cout<<"division = "<<f;
+
+
+
+    return 0;
+
+
+}
