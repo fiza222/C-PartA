d7209697f91fc9a789b2561f64604f2c6c196a4b
fiza222 <fizaparween112@gmail.com>
2026-04-03 19:32:09 +0530

    Add files via upload

---
+++ qsn4.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+   using namespace std;
+   int main(){
+
+       int a;
+       cout<<"enter a variable : ";
+       cin>>a;
+       cout<<"the variable is : "<<a;
+       return 0;
+
+   }
