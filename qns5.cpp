563568bd657cc6ecea424818585eb51bcb05c720
fiza222 <fizaparween112@gmail.com>
2026-04-03 19:37:38 +0530

    Add files via upload

---
+++ qns5.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+     using namespace std;
+
+     int main(){
+
+
+         int a,square;
+
+         cout<<"enter a number : ";
+         cin>>a;
+
+         square=a*a;
+
+         cout<<"square of number is : "<< square;
+
+
+         return 0;
+
+
+
+
+
+
+
+
+     }
