676ac0449cab0c4b74121d0dee54b4c6befe90bc
fiza222 <fizaparween112@gmail.com>
2026-04-03 19:48:05 +0530

    Add files via upload

---
+++ qns11.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+             using namespace std;
+
+             int main(){
+
+             int a,b,c;
+
+             cout<<"enter 1st number : ";
+             cin>>a;
+
+             cout<<"enter 2nd number : ";
+             cin>>b;
+
+             c=a%b;
+             cout<<"remainder of these numbers = "<<c;
+             return 0;
+
+             }
