64d59a7557cc32c67c09bc565820d91cca40f99e
fiza222 <fizaparween112@gmail.com>
2026-04-03 19:38:39 +0530

    Add files via upload

---
+++ qns7.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float celcius,fahrenheit;
+
+    cout<<"enter the temperature in celcius : ";
+    cin>>celcius;
+
+    fahrenheit=(9/5 * celcius) + 32;
+    cout<<"enter the temperature in fahrenheit: "<<fahrenheit;
+
+    return 0;
+}
